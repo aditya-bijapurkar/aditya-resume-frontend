6ef541b0e9b5e8ba27ca7f87d850fe114e57f1e1
Aditya Bijapurkar <aditya.bijapurkar@aidash.com>
2026-01-25 14:11:30 +0530

    system architecture diagram changes

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -99,7 +99,7 @@ const Overview: React.FC = () => {
             <div className="flowchart-image-wrapper">
               <h3>📊 System Architecture Flowchart</h3>
               <img 
-                src="/system-architecture-05.svg" 
+                src="/system-architecture-06.svg" 
                 alt="System Architecture Flowchart"
                 className="flowchart-image"
               />
